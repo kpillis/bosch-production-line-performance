84fe5e9a7467514e3a3af27ae5686fc5d1b53031
Krisztian Pillis <pillis.krisztian@gmail.com>
2016-11-23 17:41:01 +0100

    Join script, enhancements and bug fixes.

---
+++ scripts/joiner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+join --header -t',' -j 1 train_numeric.csv train_date.csv > train_numdate.csv
+join --header -t',' -j 1 train_numdate.csv train_categorical.csv > trainer.csv
+join --header -t',' -j 1 test_numeric.csv test_date.csv > test_numdate.csv
+join --header -t',' -j 1 test_numdate.csv test_categorical.csv > tester.csv
