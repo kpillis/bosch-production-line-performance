c5280c0fdbee45455b0c35a884d7a771cbb420e3
Krisztian Pillis <pillis.krisztian@gmail.com>
2016-11-28 00:43:00 +0100

    Use probabilities instead of classes.

--- scripts/joiner.sh
+++ scripts/joiner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd ../input
 join --header -t',' -j 1 train_numeric.csv train_date.csv > train_numdate.csv
 join --header -t',' -j 1 train_numdate.csv train_categorical.csv > trainer.csv
 join --header -t',' -j 1 test_numeric.csv test_date.csv > test_numdate.csv
 join --header -t',' -j 1 test_numdate.csv test_categorical.csv > tester.csv
+cd ../scipts
